cd646f4c53a0374be12bf5a9232237e642f63b02
setor dzisenu <sdzisenu@gmail.com>
2020-05-03 18:32:51 +0000

    add query for atx city summary create table

---
+++ 6_ atx city summary.sql
@@ -0,0 +1,7 @@
+--6
+create table atx_volume_city_summary as 
+select a2.city,
+	   sum(abs(at2.amount)) as total_volumes  
+from agent_transactions at2 join agents a2 on at2.agent_id = a2.agent_id 
+where date_part('week', at2.when_created) = date_part('week', current_date) - 1
+group by a2.city;
